20ac55c4aef1927efbcdca170060820175886443
Toeyty <61030236@kmitl.ac.th>
2019-09-14 13:43:22 +0700

    ให้เขียนโปรแกรมเพื่อสร้างตารางความจริงของลอจิกดังต่อไปนี้ ของแลป 16

--- 61030236/lab01/lab01/Program.cs
+++ 61030236/lab01/lab01/Program.cs
@@ -20,6 +20,48 @@ namespace lab01
             A = true; B = true; Y = A & B;
             Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
             Console.WriteLine("-----------------------");
+
+
+
+            Console.WriteLine("      Y = A OR B");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("   A      B\t|  Y");
+            Console.WriteLine("-----------------------");
+            A = false; B = false; Y = A & B;
+            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
+            A = false; B = true; Y = A & B;
+            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
+            A = true; B = false; Y = A & B;
+            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
+            A = true; B = true; Y = A & B;
+            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
+            Console.WriteLine("-----------------------");
+
+
+            Console.WriteLine("      Y = NOT A");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine(value: "A   \t|  Y");
+            Console.WriteLine("-----------------------");
+            A = false; Y = true; Y = A;
+            Console.WriteLine("{0}\t | {1}",A,Y);
+            A = true; Y = false; Y = A;
+            Console.WriteLine("{0}\t | {1}", A, arg1: Y);
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("-----------------------");
+
+
+            Console.WriteLine("ตัวดำเนินการ | เครื่องหมาย");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine(value: "Logical |  &");
+            Console.WriteLine(value: "Logical XOR |  ^");
+            Console.WriteLine(value: "Logical OR | |");
+            Console.WriteLine("-----------------------");
+
+
+
+
+
+
         }
     }
 }
