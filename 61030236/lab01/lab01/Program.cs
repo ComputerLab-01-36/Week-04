097a696d17f558b46e89ce124ee2d5fd7a4b8f3a
Toeyty <61030236@kmitl.ac.th>
2019-09-14 13:23:08 +0700

    14. การใช้งานข้อมูลชนิด boolean (1)

--- 61030236/lab01/lab01/Program.cs
+++ 61030236/lab01/lab01/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace lab01
+﻿using System;
+
+namespace lab01
 {
     class Program
     {
+        private const int MaxValue = 9;
+
         static void Main(string[] args)
         {
-
+            Random random = new Random();
+            int randomNumber = random.Next(0, MaxValue);
+            Console.WriteLine(randomNumber);
+            bool a = 0 > 9;
+            Console.WriteLine("{0}", a);
         }
     }
 }
