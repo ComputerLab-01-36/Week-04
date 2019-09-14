9e11bc7facb3c890d07eac3fb20b77405c61e307
Toeyty <61030236@kmitl.ac.th>
2019-09-14 14:13:01 +0700

    18. หาค่าจากสมการทางคณิตศาสตร์

--- 61030236/lab01/lab01/Program.cs
+++ 61030236/lab01/lab01/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace lab01
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab01
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter :");
+            
+            int a = 10, b = 20, x = 5, y = 2;
+            Console.WriteLine("int = a + b");
+            Console.WriteLine("int = x - b");
+            Console.WriteLine("int = x* b");
+            Console.WriteLine("int = y / a");
+            Console.WriteLine("int = b % y");
+            Console.WriteLine("int = y + 10 % x");
+            Console.WriteLine("int = a / 3 * 5");
+            Console.WriteLine("int = 9 / 2 * a");
+            Console.WriteLine("int = y % 8");
+            Console.WriteLine("int = (100 * x) + y % 2 - a");
 
         }
     }
