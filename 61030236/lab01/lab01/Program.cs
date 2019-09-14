c886a864a821601243cb5b0a78dafca73c586315
Toeyty <61030236@kmitl.ac.th>
2019-09-14 15:23:21 +0700

    19. โปรแกรมคำนวณระยะทางและเวลาของแสงจากดวงอาทิตย์ถึงโลก

--- 61030236/lab01/lab01/Program.cs
+++ 61030236/lab01/lab01/Program.cs
@@ -4,7 +4,9 @@ namespace lab01
 {
     class Program
     {
-        static void Main(string[] args)
+        public static double km { get; private set; }
+
+        private static void Main(string[] args)
         {
             const double lightSpeed = 186000d;   // miles per second
             Console.WriteLine("Light speed = {0} Mile Per second", lightSpeed);
@@ -15,6 +17,11 @@ namespace lab01
             double SunToEarthTimeOfLight = SunToEarthDistance / lightSpeed;  // miles
             Console.WriteLine("SunToEarthTimeOfLight = {0} seconds", SunToEarthTimeOfLight);
             Console.WriteLine("SunToEarthTimeOfLight = {0} minutes", SunToEarthTimeOfLight / 60d);
+
+
+
+
+
         }
     }
 }
