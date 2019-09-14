414907c8bdb559f7dc6862164f5e6e834c8a1810
Toeyty <61030236@kmitl.ac.th>
2019-09-14 13:45:25 +0700

    17. การใช้เครื่องหมายทางคณิตศาสตร์กับตัวแปรชนิดจำนวนเต็ม

--- 61030236/lab01/lab01/Program.cs
+++ 61030236/lab01/lab01/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-
+            int a, b, c, d, e, f;
+            a = 1;
+            b = a + 6;
+            c = b - 3;
+            d = c * 2;
+            e = d / 2;
+            f = e % 2;
         }
     }
 }
