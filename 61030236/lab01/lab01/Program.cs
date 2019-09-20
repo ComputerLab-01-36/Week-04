80a0f3127d09bb9cf85b8a9cb7a82f629ff90480
Toeyty <61030236@kmitl.ac.th>
2019-09-20 20:36:19 +0700

    commit

--- 61030236/lab01/lab01/Program.cs
+++ 61030236/lab01/lab01/Program.cs
@@ -6,7 +6,15 @@ namespace lab01
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
+            for (float i = -6; i < Math.PI * 2.0F; i += 0.3F)
+            {
+                Console.WriteLine("The Palabola of {0,10:F} = {1,-10:F6}" + spaces(Math.Pow(i, 2)) + "*", i, Math.Pow(i, 2));
+            }
+        }
+        private static string spaces(double val)
+        {
+            string SpaceString = new String(' ', ((int)(val * 10.0)) + 10);
+            return SpaceString;
         }
     }
 }
